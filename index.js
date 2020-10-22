b348b8289e9a697d9bc2c58806d5f5c02acce8a1
Eunkyo Seo <freehunterc@gmail.com>
2020-10-22 22:31:41 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require("express");  
+const bodyParser = require("body-parser");  
+const fs = require("fs");
+const cookieParser = require('cookie-parser');
+const app = express();  
+  
+app.use(bodyParser.urlencoded({ extended:  false  }));  
+app.use(bodyParser.json()); 
+app.use(cookieParser()); 
+  
+
+var userInfo = {
+
+}
+
+
+app.get('/',function (req,res) {  
+    
+
+    var name = 'guest';
+    var youtubeAddr = 'https://www.youtube.com/embed/v64KOxKVLVg';
+
+    if(req.cookies.uname){
+        name = req.cookies.uname;
+        if(userInfo[name])
+            youtubeAddr = 'https://www.youtube.com/embed/'+userInfo[name];
+
+    }
+       
+
+    fs.readFile(__dirname + '/view/index.html', 'UTF-8',
+         (err, data) => {
+            var conv_data = data.replace(/#name#/g, name).replace(/#youtubeAddr#/g, youtubeAddr);
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(conv_data);
+            res.end();
+        }
+    )
+
+ 
+});  
+
+app.get('/userinfo',function (req,res) {  
+
+    //console.log(req.query.youtube_addr)
+    const urlsplited = req.query.youtube_addr.split('/');
+    userInfo[req.query.uname] = urlsplited[urlsplited.length-1];
+    
+    console.log(userInfo);
+
+    res.cookie('uname', req.query.uname,{
+        maxAge:1000000
+     });
+   
+   
+    res.redirect('/');  
+});
+//removecookie
+app.get('/removecookie',function (req,res) {  
+    console.log('removecookie')
+    res.clearCookie('uname');
+    res.redirect('/');  
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
